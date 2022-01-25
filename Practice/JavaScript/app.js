7cc032b613fa59d7c5a58a3f55ccaeb4c2aa740f
nklsmbailey <nklsmbailey@gmail.com>
2022-01-25 09:49:38 -0600

    commit message

---
+++ Practice/JavaScript/app.js
@@ -0,0 +1,12 @@
+var friend = {
+    name: 'Nicholas',
+    age: 58,
+    address: {
+        address1: '586 Turtle Creek',
+        city: 'Hoover',
+        state: 'AL'
+    },
+};
+
+
+console.log(friend.address.state);
